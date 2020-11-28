bd85a9f8f8bdcf4db3ae752265db5a3211838a37
Elaine Parr <parr.mi@husky.neu.edu>
2020-11-28 17:46:14 -0500

    add apikey to random function

--- src/services/RecipeService.js
+++ src/services/RecipeService.js
@@ -6,7 +6,7 @@ const baseUrl = "https://api.spoonacular.com/recipes"
 const recipeDetailsSecondHalf = "information?includeNutrition=false&apiKey=fd8eb1342ad14b99aa1933816c38d9fe"
 
 export const findRandomRecipes = () =>
-    fetch(`${baseUrl}/random?number=16`).then(response=>response.json())
+    fetch(`${baseUrl}/random?number=16&apiKey=${apiKey}`).then(response=>response.json())
 
 export const searchRecipes = (query) =>
     fetch(`${searchRecipesUrl}/?query=${query}?includeInstructions=true&apiKey=${apiKey}`).then(response=>response.json())
