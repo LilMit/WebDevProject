de40f9779aa2fc6cf9c3679b57275f96c02e3486
Elaine Parr <parr.mi@husky.neu.edu>
2020-12-13 11:37:36 -0500

    update ingredients component to read ingredients from string

--- wbdv-fa20-project-client-react/src/components/RecipeContent/IngredientsComponent.js
+++ wbdv-fa20-project-client-react/src/components/RecipeContent/IngredientsComponent.js
@@ -5,10 +5,13 @@ const IngredientsComponent = ({recipe}) => {
     return (
         <div className="col">
             <h3>Ingredients</h3>
-            <ul className="list-group">{
-                recipe.extendedIngredients.map(
-                    extendedIngredient => <li className="list-group-item">{extendedIngredient.originalString}</li>)
-            }</ul>
+            {/*<ul className="list-group">{*/}
+            {/*    recipe.extendedIngredients.map(*/}
+            {/*        extendedIngredient => <li className="list-group-item">{extendedIngredient.originalString}</li>)*/}
+            {/*}</ul>*/}
+            <ul className="list-group">
+                {splitText(recipe.ingredients).map(line=><li className="list-group-item">{line}</li>)}
+            </ul>
         </div>
     )
 }
@@ -17,4 +20,6 @@ const mapStateToProps = (state) => ({
     // ingredient: state.recipeReducer.ingredient,
     // extendedIngredient: state.recipeReducer.extendedIngredient
 })
+
+const splitText = (ingredients) => ingredients.split("\n");
 export default connect(mapStateToProps) (IngredientsComponent)
